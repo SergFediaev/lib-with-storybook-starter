fc0de2bf6b561a848c45210aa99e6cc30ea92042
SergFediaev <sergfediaev@gmail.com>
2024-07-11 15:44:37 +0300

    Config project.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
 import { defineConfig } from 'vite'
+
+import { dependencies, devDependencies } from './package.json'
+// ...Object.keys(peerDependencies)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // vite.config.js
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      // the proper extensions will be added
+      fileName: 'index',
+      formats: ['es'],
+      name: 'lib-starter',
+    },
+    rollupOptions: {
+      external: [
+        ...Object.keys(dependencies),
+        ...Object.keys(devDependencies),
+        'react/jsx-runtime',
+      ],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        // globals: {
+        //   vue: 'Vue',
+        // },
+      },
+    },
+    sourcemap: true,
+    target: 'esnext',
+  },
 })
